0f7ff51902f2a51851014c5aa831ecadf563fccf
Parthibg00410769 <g00410769@atu.ie>
2023-10-18 13:17:01 +0100

    Week4 Registration service part has been added

---
+++ src/main/java/com/ie/atu/RegisterController.java
@@ -0,0 +1,13 @@
+package com.ie.atu;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegisterController {
+    @PostMapping("/confirm")
+    public String confirmDetails(@RequestBody UserDetails userDetails){
+        return String.format("Received details for %s with email %s", userDetails.getName(), userDetails.getEmail());
+    }
+}
